7852bdfd95b035378bb34e93e950e6e143785ae5
Michael Nikitochkin <miry.sof@gmail.com>
2020-03-12 22:04:28 +0100

    Specify how to find the HEAD sources

--- Formula/medup.rb
+++ Formula/medup.rb
@@ -1,6 +1,7 @@
 class Medup < Formula
   desc "Export Medium articles to Markdown"
   homepage "http://github.com/miry/medup"
+  head "https://github.com/miry/medup.git"
   url "https://github.com/miry/medup/archive/v0.1.6.tar.gz"
   sha256 "b53da407a6698d491858814e8b1bf9645970bcf88b5c3b7fe13e09f125737933"
   version "0.1.6"
