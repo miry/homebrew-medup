5d123f05e83fc46bb0fa391244015940106d4de8
Michael Nikitochkin <miry.sof@gmail.com>
2020-05-22 23:18:27 +0200

    Upgrade medup to 0.1.8

--- Formula/medup.rb
+++ Formula/medup.rb
@@ -2,13 +2,13 @@ class Medup < Formula
   desc "Export Medium articles to Markdown"
   homepage "http://github.com/miry/medup"
   head "https://github.com/miry/medup.git"
-  url "https://github.com/miry/medup/archive/v0.1.6.tar.gz"
-  sha256 "b53da407a6698d491858814e8b1bf9645970bcf88b5c3b7fe13e09f125737933"
-  version "0.1.6"
-  depends_on "crystal-lang"
+  url "https://github.com/miry/medup/archive/v0.1.8.tar.gz"
+  sha256 "759a31c1a3a9fbb7951fe81d564daf719c75f0c86582201c07dcebde9538421a"
+  version "0.1.8"
+  depends_on "crystal"
 
   def install
-    system "crystal build --release -o medup src/cli.cr"
+    system "crystal build --release --no-debug -o medup src/cli.cr"
     bin.install "medup"
   end
 
