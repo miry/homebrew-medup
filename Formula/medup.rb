3091c8eda07fd00c2ccd1f5f3255adc37ca05d97
Michael Nikitochkin <michael.nikitochkin@gmx.net>
2022-03-27 20:34:04 +0200

    Release v0.2.1

--- Formula/medup.rb
+++ Formula/medup.rb
@@ -2,9 +2,9 @@ class Medup < Formula
   desc "Export Medium articles to Markdown"
   homepage "http://github.com/miry/medup"
   head "https://github.com/miry/medup.git"
-  url "https://github.com/miry/medup/archive/v0.2.0.tar.gz"
-  sha256 "5f140182bfa6c69debeb1a2c1d08694fba88f7caed5721e7f8a0cca11804e6b7"
-  version "0.2.0"
+  url "https://github.com/miry/medup/archive/v0.2.1.tar.gz"
+  sha256 "1c8a25af3498d963d4950e8f61af8a260fd2fa0e623e1aee2732e1cb8e8526d4"
+  version "0.2.1"
   depends_on "crystal"
 
   def install
